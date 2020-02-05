ae0b905f5d1a2118f9baeeb9dec92d60290742e3
Kyle Kilby <kyle.lee.kilby@gmail.com>
2020-02-05 15:42:53 -0800

    testing

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,37 @@
 var Stack = function() {
   var someInstance = {};
-
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //keys are numbered sequentially
+    //values are value inputted
+    //set key val pairs in storage obj
+    storage[count] = value;
+    //increment count variable
+    count++;
   };
 
   someInstance.pop = function() {
+    //check if stack is empty
+    if (count === 0) {
+      return 0;
+    } else {
+      //change count - 1
+      count--;
+      var popped = storage[count];
+      delete popped;
+      return popped;
+    }
   };
 
   someInstance.size = function() {
+    // var storageLength = Object.keys(storage).length;
+    // return storageLength;
+    //or just return count 0, if empty
+    return count;
   };
 
   return someInstance;
