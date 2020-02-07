bd3aade3bf392eae7ab98d23406dba905e83c93d
Kyle Kilby <kyle.lee.kilby@gmail.com>
2020-02-07 10:59:13 -0800

    solution to es6 complete

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,39 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
   }
 
+  //push,pop,size functions
+  //method to add to stack
+  push(value) {
+    //set key val pairs in this.storage obj
+    this.storage[this.count] = value;
+    //increment this.count variable
+    this.count++;
+  }
+
+  //method to delete from stack
+  pop() {
+    //check if stack is empty
+    if (this.count < 1) {
+      return 0;
+      //else greater than or equal to 1
+    } else {
+      //decrement count
+      this.count--;
+      var popped = this.storage[this.count];
+      delete this.storage[this.count];
+      return popped;
+    }
+  }
+
+  //method to track size of storage
+  size() {
+    return Object.keys(this.storage).length;
+    // return count;
+  }
 }
+
+var callStack = new Stack();
