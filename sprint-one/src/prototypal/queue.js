56048f2402a84a85c3e3268f88e748c05841d1a7
Kyle Kilby <kyle.lee.kilby@gmail.com>
2020-02-06 23:21:57 -0800

    solution to prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,42 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  return someInstance;
 };
 
 var queueMethods = {};
 
+//create a count variable
+var count = 0;
+//push,pop,size functions
+//method to add to stack
+queueMethods.enqueue = function(value) {
+  //set key val pairs in this.storage obj
+  this.storage[count] = value;
+  //increment this.count variable
+  count++;
+};
+//create a func to find the smallest num
+queueMethods.smallest = function() {
+  var small = count;
+  for (var key in this.storage) {
+    if (key < small) {
+      small = key;
+    }
+  }
+  return small;
+};
+//method to delete from stack
+queueMethods.dequeue = function() {
+  var deqe = this.storage[this.smallest()];
+  delete this.storage[this.smallest()];
+  return deqe;
+};
 
+//method to track size of storage
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+  // return count;
+};
