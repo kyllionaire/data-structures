f626d98f55f8ebac06c49f222ba0801bd4a90025
Kyle Kilby <kyle.lee.kilby@gmail.com>
2020-02-07 10:59:54 -0800

    solution to es6 complete

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,39 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
   }
 
+  // enqueue(string),dequeue(),size()
+  enqueue(value) {
+    //set key val pairs in this.storage obj
+    this.storage[this.count] = value;
+    //increment this.count variable
+    this.count++;
+  }
+  //create a func to find the smallest num
+  smallest() {
+    var small = this.count;
+    for (var key in this.storage) {
+      if (key < small) {
+        small = key;
+      }
+    }
+    return small;
+  }
+  //method to delete from stack
+  dequeue() {
+    var deqe = this.storage[this.smallest()];
+    delete this.storage[this.smallest()];
+    return deqe;
+  }
+
+  //method to track size of storage
+  size() {
+    return Object.keys(this.storage).length;
+    // return count;
+  }
 }
+
+var callQueue = new Queue();
